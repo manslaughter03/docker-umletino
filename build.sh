c179dfe3c172ceec0f678e580fe6f49413f6b406
manslaughter <manslaughter03@gmail.com>
2017-08-18 01:34:58 +0200

    add raspberry support

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 REPO="umletino"
 TAG="alpine"
+ARCH="$(uname -m)"
+
+DOCKERFILE="Dockerfile"
+
+if [[ $string == *"arm"* ]]; then
+  DOCKERFILE+=".rpi" 
+fi
 
 REPO_TMP="gwt-builder"
 
@@ -9,10 +16,10 @@ set -ex
 
 rm -rf ./html/* && mkdir -p ./html
 
-docker build -t $REPO_TMP -f gwt-builder/Dockerfile ./gwt-builder
+docker build -t $REPO_TMP -f gwt-builder/$DOCKERFILE ./gwt-builder
 TMP_ID=$(docker run -d $REPO_TMP)
 docker cp $TMP_ID:/tmp/html .
 docker rm -f $TMP_ID
 docker rmi $REPO_TMP
 
-docker build -t $REPO:$TAG -f Dockerfile .
+docker build -t $REPO:$TAG -f $DOCKERFILE .

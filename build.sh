e820ae069c9846da9347139a4e794a26db44bc34
Nicolas Bernard <nicolas.bernard@consertotech.pro>
2017-11-26 14:43:39 +0100

    test docker hub ci

--- build.sh
+++ build.sh
@@ -14,12 +14,9 @@ REPO_TMP="gwt-builder"
 
 set -ex
 
-rm -rf ./html/* && mkdir -p ./html
-
-docker build -t $REPO_TMP -f gwt-builder/$DOCKERFILE ./gwt-builder
-TMP_ID=$(docker run -d $REPO_TMP)
-docker cp $TMP_ID:/tmp/html .
-docker rm -f $TMP_ID
-docker rmi $REPO_TMP
+docker build -t $REPO_TMP -f ${DOCKERFILE}.build .
+docker create --name build $REPO_TMP
+docker cp build:./html .
+docker rm -f build
 
 docker build -t $REPO:$TAG -f $DOCKERFILE .
